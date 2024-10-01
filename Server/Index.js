959ad208452ab702a69194de42076fc7b3b5e809
Sultana Rupa <147791044+AsTeriaa09@users.noreply.github.com>
2024-10-01 10:40:19 +0600

    Update Index.js

--- Server/Index.js
+++ Server/Index.js
@@ -49,35 +49,7 @@ app.post("/payment", async (req, res) => {
     }
 });
 
-// app.post("/payment", (req, res) => {
-//     const { product, token } = req.body;
-//     console.log("Product", product);
-//     console.log("price", product.price);
-//     const idempotencyKey = uuidv4();
-
-//     return stripe.customers.create({
-//         email: token.email,
-//         source: token.id
-//     }).then(customer => {
-//         stripe.charges.create({
-//             amount: product.price * 100,
-//             currency: 'usd',
-//             customer: customer.id,
-//             receipt_email: token.email,
-//             description: product.name,
-//             shipping: {
-//                 name: token.card.name,
-//                 address: { country: token.card.address_country }
-//             }
-//         }, { idempotencyKey })
-//     }).then(result => {
-//         console.log("Charge created:", charge.id);
-//         res.status(200).json(charge);
-//     }).catch(err => {
-//         res.status(500).json({ msg: err });
-//     })
-// })
-
+           
 
 // listen
 const Port = process.env.port;
